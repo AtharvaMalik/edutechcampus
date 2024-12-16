1b7c8c6fec11bb8f064aa6da866baf84274a5cf5
Atharva Malik <atharva@sjpdamla.ac.in>
2024-12-16 14:02:50 +0530

    1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="bg-gradient-to-r from-red-400 to-red-500 h-[80px] flex items-center justify-center">
-      <div className="w-[90%] bg-red-200 bg-opacity-30 rounded-xl shadow-lg flex items-center justify-between px-6 py-2">
+    <div className=" flex items-center justify-center xl:mt-8">
+      <div className="w-[90%] bg-white bg-opacity-25 rounded-xl shadow-lg flex items-center justify-between px-6 py-2">
         {/* Left Side */}
-        <div className="text-white text-xl font-cursive">
-          EduTechCampus
+        <div className="font-lovers text-white text-xl xl:text-4xl">
+          EdutechCampus
         </div>
 
         {/* Right Side */}
-        <div className="flex space-x-8 text-white font-medium">
+        <div className="flex space-x-24 text-white font-medium font-Helvetica">
           <a href="#" className="hover:text-red-200 transition">Home</a>
           <a href="#" className="hover:text-red-200 transition">Services</a>
           <a href="#" className="hover:text-red-200 transition">About Us</a>
