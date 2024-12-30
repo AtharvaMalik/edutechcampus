a6b021a8214f49112235017bd77869aed8c4f28b
AtharvaMalik <atharvamalik28@gmail.com>
2024-12-31 01:43:01 +0530

    responisve

--- components/Herocontent.tsx
+++ components/Herocontent.tsx
@@ -11,11 +11,11 @@ const HeroContent: React.FC = () => {
                         className="xl:mb-0 -z-1 absolute speak"
                     />
                     <div className="HeroHeading text-center">
-                        <h1 style={{ fontSize: "5rem", fontWeight: "bold", marginTop: "0" }}>Edutech Campus</h1>
+                        <h1 className="text-7xl font-bold mt-7 xl:mt-12">Edutech Campus</h1>
                     </div>
                 </div>
-                <h3 className="SubHead text-center p-2">Your Trusted Computer Service Center in Jagadhri</h3>
-                <p className="literacy text-center p-2">Digital Literacy , Government Services, Utility Payments, and More</p>
+                <h3 className="SubHead text-center p-10xl :mt-3 xl:p-1">Your Trusted Computer Service Center in Jagadhri</h3>
+                <p className="literacy text-center xl:mt-1">Digital Literacy , Government Services, Utility Payments, and More</p>
                 <button className="text-white text-center font-libre-franklin text-xl leading-tight rounded-lg bg-gray-200 bg-opacity-35 shadow-md py-2.5 px-5 mt-5">
                     Explore Our Services
                 </button>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 
 const Navbar = () => {
-    
   return (
-
-    <div className="flex items-center justify-center sm:mt-5 xl:mt-5">
-      <div className="w-auto xl:w-[85%] bg-white bg-opacity-25 rounded-xl shadow-lg flex items-center justify-between px-6 py-2">
+    <div className="flex items-center justify-center mt-5 sm:mt-10 xl:mt-5">
+      <div className="w-auto xl:w-[85%] bg-white bg-opacity-25 rounded-xl shadow-lg flex flex-col xl:flex-row items-center justify-between px-6 py-2">
         {/* Left Side */}
-        <div className="font-lovers text-white text-xl xl:text-4xl ">
+        <div className="font-lovers text-white text-4xl xl:text-4xl mb-4 xl:mb-0">
           EdutechCampus
         </div>
 
         {/* Right Side */}
-        <div className="flex space-x-24 text-white font-medium font-Helvetica">
+        <div className="flex flex-row xl:flex-row space-x-3 xl:space-y-0 xl:space-x-24 text-white font-medium font-Helvetica">
           <a href="#" className="hover:text-red-200 transition">Home</a>
           <a href="#" className="hover:text-red-200 transition">Services</a>
           <a href="#" className="hover:text-red-200 transition">About Us</a>
