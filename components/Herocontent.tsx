0832085e0319b0684bef72d231b9dbc504f66544
AtharvaMalik <atharvamalik28@gmail.com>
2025-01-03 18:07:45 +0530

    button

--- components/Herocontent.tsx
+++ components/Herocontent.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
+import { TransitionLink } from '@/lib/transitionLink';
 
 const HeroContent: React.FC = () => {
     return (
         <div className="HeroContent xl:-mt-40">
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center p-1">
                 <div className="relative flex justify-center">
                     <Image
                         src="/Speak.png" 
@@ -15,14 +16,16 @@ const HeroContent: React.FC = () => {
                         layout="none"
                     />
                     <div className="HeroHeading text-center">
-                        <h1 className="text-7xl heightforedutech xl:leading-tight font-bold mt-8 xl:-mt-12">Edutech Campus</h1>
+                        <h1 className="text-6xl heightforedutech xl:leading-tight font-bold mt-8 xl:mt2">Edutech Campus</h1>
                     </div>
                 </div>
-                <h3 className="SubHead text-center p-10xl mt-5 xl:-mt-2 xl:p-1">Your Trusted Computer Service Center in Jagadhri</h3>
+                <h3 className="SubHead text-center p-10xl mt-2 xl:-mt-2 xl:p-1">Your Trusted Computer Service Center in Jagadhri</h3>
                 <p className="literacy text-center xl:mt-1">Digital Literacy , Government Services, Utility Payments, and More</p>
-                <button className="text-white text-center font-libre-franklin text-xl leading-tight rounded-lg bg-gray-200 bg-opacity-35 shadow-md py-2.5 px-5 mt-5">
-                    Explore Our Services
-                </button>
+                <div className="mt-5 text-white text-center font-libre-franklin text-xl leading-tight rounded-lg bg-gray-200 bg-opacity-35 shadow-md py-2.5 px-5">
+                    <TransitionLink href="/services">
+                        Explore Our Services
+                    </TransitionLink>
+                </div>
             </div>
         </div>
     );
